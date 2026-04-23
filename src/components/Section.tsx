48a94e57c277518bc104cd6dc6a1f87cf35faddf
Niharika Sunwar <niharika@Yogini.localdomain>
2026-04-23 13:42:43 +0100

    Changed fade in time

--- src/components/Section.tsx
+++ src/components/Section.tsx
@@ -23,7 +23,7 @@ const Section = ({
             initial={{ opacity: 0, y: 40 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.1 }}
-            transition={{ duration: 0.2, ease: 'easeOut' }}
+            transition={{ duration: 0.6, ease: 'easeOut' }}
         >
             <GlowingBadge label={label}/>
             {children}
